a4c5d4bb6b46be59eda6b5401de0895083b723b4
Shweta Jain <shweta.silver06@gmail.com>
2019-11-14 11:28:01 +0530

    Remove target

--- spring/spring-batch-sample/target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE people IF EXISTS;
-
-CREATE TABLE product  (
-    person_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
-    product_name VARCHAR(2000),
-    product_description VARCHAR(2000),
-    pRice NUMBER(100)
-);
